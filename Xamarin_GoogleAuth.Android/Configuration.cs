0a6c0a86e85ecbe1a29430dfa9f07c4bf2eb12db
Zupirio <zupirio@gmail.com>
2018-05-28 11:35:37 +0200

    Google sign in

---
+++ Xamarin_GoogleAuth.Android/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace Xamarin_GoogleAuth.Droid
+{
+    public static class Configuration
+    {
+        public const string ClientId = "1086435689220-30g5dv9tkgg6ifrcme5shc0j8bdgfk65.apps.googleusercontent.com";
+        public const string Scope = "email";
+        public const string RedirectUrl = "com.bcx.login:/oauth2redirect";
+    }
+}
---
+++ Xamarin_GoogleAuth.iOS/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Xamarin_GoogleAuth.iOS
+{
+    public static class Configuration
+    {
+        public const string ClientId = "10688662461-roi0fgup3fdtenr8s462rurt0auov42o.apps.googleusercontent.com";
+        public const string Scope = "email";
+        public const string RedirectUrl = "https://loginappg-194713.firebaseapp.com/__/auth/handler";
+    }
+}
